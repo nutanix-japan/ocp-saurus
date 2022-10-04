9b3ead41010e983621f353f5b6c80d27d84d6f3a
Nutanix Japan <lakshmi.balaramane@nutanix.com>
2022-10-04 15:05:20 +0900

    Tue Oct  4 15:05:20 JST 2022

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       type: 'category',
       label: 'OCP with Nutanix Cloud Manager (NCM)',
-      items: ['ocp_ntnx_hci/import_bp','ocp_ntnx_hci/prov_vm','ocp_ntnx_hci/ocp_vms','ocp_ntnx_hci/ocp_scale_out_in','ocp_ntnx_hci/ocp_csi','optional_labs/ocp_image_registry'],
+      items: ['ocp_ntnx_hci/import_bp','ocp_ntnx_hci/prov_vm','ocp_ntnx_hci/ocp_vms','ocp_ntnx_hci/ocp_scale_out_in'],
     },
     {
       type: 'category',
@@ -15,6 +15,11 @@ module.exports = {
       type: 'category',
       label: 'OCP with Assisted Installer',
       items: ['ocp_assisted_install/ocp_assisted_install'],
+    },
+    {
+      type: 'category',
+      label: 'Nutanix Storage CSI',
+      items: ['ocp_ntnx_hci/ocp_csi','optional_labs/ocp_image_registry'],
     },
     {
       type: 'category',
