4b381c2ca1c4bf525007afaa26bd35057681b8a5
Nutanix Japan <lakshmi.balaramane@nutanix.com>
2022-12-14 12:17:06 +0900

    Wed Dec 14 12:17:06 JST 2022

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,17 @@
 module.exports = {
   tutorialSidebar:[
     "intro",
+    "ocp_ipi_install/ocp_ipi_install",
     {
       type: 'category',
-      label: 'OCP with Nutanix Cloud Manager (NCM)',
-      items: ['ocp_ntnx_hci/import_bp','ocp_ntnx_hci/prov_vm','ocp_ntnx_hci/ocp_vms','ocp_ntnx_hci/ocp_scale_out_in'],
+      label: 'Nutanix Storage CSI',
+      items: ['ocp_ntnx_hci/ocp_csi','optional_labs/ocp_image_registry'],
     },
+    "ocp_ndb/ocp_ndb",
     {
       type: 'category',
-      label: 'OCP with IPI Installer',
-      items: ['ocp_ipi_install/ocp_ipi_install'],
+      label: 'OCP Workload Backup/Restore',
+      items: ['optional_labs/objects_prep','workloads_on_ocp/ocp_k10','workloads_on_ocp/ocp_wordpress_deploy','workloads_on_ocp/ocp_k10_backup_restore'],
     },
     {
       type: 'category',
@@ -18,23 +20,13 @@ module.exports = {
     },
     {
       type: 'category',
-      label: 'Nutanix Storage CSI',
-      items: ['ocp_ntnx_hci/ocp_csi','optional_labs/ocp_image_registry'],
-    },
-    {
-      type: 'category',
-      label: 'Workloads on OCP',
-      items: ['optional_labs/objects_prep','workloads_on_ocp/ocp_k10','workloads_on_ocp/ocp_wordpress_deploy','workloads_on_ocp/ocp_k10_backup_restore'],
+      label: 'OCP with Nutanix Cloud Manager (NCM)',
+      items: ['ocp_ntnx_hci/import_bp','ocp_ntnx_hci/prov_vm','ocp_ntnx_hci/ocp_vms','ocp_ntnx_hci/ocp_scale_out_in'],
     },
     {
       type: 'category',
       label: 'OCP Optional Labs',
       items: ['optional_labs/ocp_vms_2','optional_labs/ocp_idp'],
-    },
-    {
-      type: 'category',
-      label: 'OCP and Nutanix NDB',
-      items: ['ocp_ndb/ocp_ndb'],
     },
     {
       type: 'category',
